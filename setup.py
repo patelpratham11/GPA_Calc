736681710dbd32548c433ddb3609c44b9fbbc185
patelpratham11 <ppatel011102@gmail.com>
2021-05-14 15:13:56 -0400

    misc.

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from setuptools import setup
 
-APP = ['testapp.py']
-DATA_FILES = ['semester.txt']
+APP = ['Grades.py']
 OPTIONS = {
  'iconfile':'icon.icns',
  'argv_emulation': True,
- 'packages': ['certifi'],
 }
 
 setup(
     app=APP,
-    data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
